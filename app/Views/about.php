d423de8a2880fcb966af1ad4a46b54a5d21920b6
Naia <bosqueennaia@gmail.com>
2024-04-10 14:11:56 +0700

    first commit

---
+++ app/Views/about.php
@@ -0,0 +1,3 @@
+<!-- isi about.php -->
+<h1>About Us</h1>
+<p>This is the about page content.</p>
---
+++ app/Views/layouts/head.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Burger Bun - Restaurant Category Responsive Web Template | About : W3layouts</title>
+
+    <link href="//fonts.googleapis.com/css2?family=Dosis:wght@300;400;500;600;800&display=swap" rel="stylesheet">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style-starter.css">
+  </head>
+  <body>
